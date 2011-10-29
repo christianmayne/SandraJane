5883e5fb1ef7ed102cb135be43b3e7f113eba1e3
Christian Mayne <christian@christian-mayne.net>
2011-10-29 14:14:19 +0100

    Created stub suppresseshop plugin.  WIP.

---
+++ wp-content/plugins/suppress-eshop/suppresseshop.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Plugin Name: Suppress eShop 
+Plugin URI: http://
+Description: Used to suppress eShop tags.  Will be used to supress when coming from a specified domain, eventually
+Version: 0.1
+Author: Christian Mayne
+Author URI: http://www.christian-mayne.net 
+License: 
+*/
+
+function display_array($array){
+	echo '<pre>'.print_r($array,1).'</pre>';
+}
+
+function supress_eshop_tags($content) {
+    get_page($pageId);
+    $eshopStrings = array('[eshop_list_subpages records="all"]');
+    $content=str_ireplace($eshopStrings,'',$content);
+    return $content;
+}
+
+add_filter('the_content', 'supress_eshop_tags');
+
+?>
