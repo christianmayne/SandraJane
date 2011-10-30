eda73e27b7ac7728bbb63d8c4a80427fa6248fc4
Peter Moore <pete.s.m@gmail.com>
2011-10-30 20:31:44 +0000

    Created Sandra Jane Theme

---
+++ wp-content/themes/sandrajane/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes() ?>>
+<head profile="http://gmpg.org/xfn/11">
+	<title><?php wp_title( '-', true, 'right' ); echo wp_specialchars( get_bloginfo('name'), 1 ) ?></title>
+	<meta http-equiv="content-type" content="<?php bloginfo('html_type') ?>; charset=<?php bloginfo('charset') ?>" />
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url') ?>" />
+<?php wp_head() // For plugins ?>
+	<link rel="alternate" type="application/rss+xml" href="<?php bloginfo('rss2_url') ?>" title="<?php printf( __( '%s latest posts', 'sandbox' ), wp_specialchars( get_bloginfo('name'), 1 ) ) ?>" />
+	<link rel="alternate" type="application/rss+xml" href="<?php bloginfo('comments_rss2_url') ?>" title="<?php printf( __( '%s latest comments', 'sandbox' ), wp_specialchars( get_bloginfo('name'), 1 ) ) ?>" />
+	<link rel="pingback" href="<?php bloginfo('pingback_url') ?>" />
+	
+	<!--[if !IE 7]>
+	<style type="text/css">
+		#wrap {display:table;height:100%}
+	</style>
+	<![endif]-->
+</head>
+
+<body class="<?php sandbox_body_class() ?>">
+
+<div id="wrapper">
+
+	<div id="header">
+		<div id="socialmedialinks"><a href="https://twitter.com/sandrajaneuk" target="_blank"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/twittericon.png"></a><a href="https://www.facebook.com/pages/Sandra-Jane/115173121836806" target="_blank"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/facebookicon.png"></a></div>		
+	</div><!--  #header -->
+
+	
