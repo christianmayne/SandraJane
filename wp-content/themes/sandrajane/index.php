48e1352a1ba852c7e6c37ca918f8b662ab2b6dc9
Peter Moore <pete.s.m@gmail.com>
2011-11-03 08:49:32 +0000

    Created template files for Furniture and Fabrics & Wallpaper. Styled leftbar menu

--- wp-content/themes/sandrajane/index.php
+++ wp-content/themes/sandrajane/index.php
@@ -9,10 +9,12 @@
 	</div><!-- #leftbar -->
 		<div id="content">
 			<h1 class="mainh1">Welcome</h1>
+			<div id="post">
 			<p>Welcome to Sandra Jane, the high quality interior design store. You'll find everything to make your home feel special; fabrics, wall coverings, soft furnishings, furniture (for inside and out) lighting and accessories.<br /><br />
 			We're happy to visit your home or business to advise on the creating and implementing of the right look.<br /><br />
 			We stock the up-market brand names you know and trust plus a host of unusual items that we have sourced from our favourite suppliers from around the world.<br /><br /> 
 			We are always introducing new stock from different suppliers and for new seasons so we don't produce a catalogue. Come and see us! We're a family business and the founders are still active in the business and keen to help you.</p>
+			</div>
 			<h1 class="mainh1">01223 323211 | <a href="mailto:contact@sandrajane.co.uk">contact@sandrajane.co.uk</a></h1>
 			<div id="decoration"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/Home.gif" class="left"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/home1.gif" class="right"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/home2.gif" class="right"></div>
 		</div><!-- #content -->
